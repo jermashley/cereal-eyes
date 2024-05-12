849f3a563cfe2cbd973671a6f1a68509c9f9fb29
Jeremiah Ashley <jeremiah.ashley@live.com>
2024-05-11 23:11:52 -0500

    Add Posthog

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,15 @@
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import posthog from 'posthog-js'
 import { createApp, h } from 'vue'
 
 import { ZiggyVue } from '../../vendor/tightenco/ziggy'
 
-const appName = import.meta.env.VITE_APP_NAME || `Laravel`
+const appName = import.meta.env.VITE_APP_NAME || `Cereal Eyes`
+
+posthog.init(`phc_5xZgAN1Xse3EySw6p5hci2IyKvV3jWb0nfeSqNnrybz`, {
+  api_host: `https://us.i.posthog.com`,
+})
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
