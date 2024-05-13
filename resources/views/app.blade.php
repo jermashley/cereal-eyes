33a29d9c330781b78b3d4e75910dccc310343c96
Jeremiah Ashley <jeremiah.ashley@live.com>
2024-05-13 16:21:22 -0500

    Add meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'Cereal Eyes') }}</title>
+    <meta name="title" content="{{ config('app.name', 'Cereal Eyes') }}" />
+    <meta name="description" content="Decode and encode your data with ease!" />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ config('app.url', 'https://cerealeyes.me') }}" />
+    <meta property="og:title" content="{{ config('app.name', 'Cereal Eyes') }}" />
+    <meta property="og:description" content="Decode and encode your data with ease!" />
+    <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="{{ config('app.url', 'https://cerealeyes.me') }}" />
+    <meta property="twitter:title" content="{{ config('app.name', 'Cereal Eyes') }}" />
+    <meta property="twitter:description" content="Decode and encode your data with ease!" />
+    <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+    <!-- Meta Tags Generated with https://metatags.io -->
+
+    <title inertia>{{ config('app.name', '{{ config('app.name', 'Cereal Eyes') }}') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
