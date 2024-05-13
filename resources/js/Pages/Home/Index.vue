f68781710e187ddf23876d05775e3b87e8575db2
Jeremiah Ashley <jeremiah.ashley@live.com>
2024-05-13 15:18:26 -0500

    Fix some stuff on the home page

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,12 +1,32 @@
 <script setup>
 import AuthGithubButton from '@/Components/feature/auth/AuthGithubButton.vue'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/Components/ui/tooltip'
 import AppLayout from '@/Layouts/AppLayout.vue'
 </script>
 
 <template>
-  <AppLayout>
-    <div class="flex flex-row items-center justify-center py-24">
-      <AuthGithubButton />
-    </div>
-  </AppLayout>
+  <TooltipProvider>
+    <AppLayout>
+      <div class="flex flex-col items-center justify-center space-y-12 py-24">
+        <h1 class="text-3xl font-semibold text-zinc-800 dark:text-zinc-200">
+          Decode
+          <Tooltip>
+            <TooltipTrigger class="line-through decoration-rose-600 opacity-50">
+              and encode
+            </TooltipTrigger>
+
+            <TooltipContent>Coming soon!</TooltipContent>
+          </Tooltip>
+          your data with ease
+        </h1>
+
+        <AuthGithubButton />
+      </div>
+    </AppLayout>
+  </TooltipProvider>
 </template>
